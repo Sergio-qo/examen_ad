0c2b4addee715574456863588516737b82c807b2
Sergio-qo <sergio.qo000@gmail.com>
2019-10-03 20:01:40 +0200

    Subo web api

---
+++ place_my_bet/PlaceMyBet/PlaceMyBet/Models/Evento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaceMyBet.Models
+{
+    public class Evento
+    {
+        public Evento(int id, string equipov, string equipol)
+        {
+            EventoId = id;
+            EquipoL = equipol;
+            EquipoV = equipov;
+        }
+        public int EventoId { get; set; }
+        public string EquipoL { get; set; }
+        public string EquipoV { get; set; }
+    }
+}
---
+++ place_my_bet/PlaceMyBet/PlaceMyBet/Models/EventosRepository.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlaceMyBet.Models
+{
+    public class EventosRepository
+    {
+        private MySqlConnection Connect()
+        {
+            string connString = "Server=localhost;Port=3306;Database=mydb;Uid=root;password=;SslMode=none";
+            MySqlConnection con = new MySqlConnection(connString);
+            return con;
+        }
+        internal List<Evento> Retrieve()
+        {
+            MySqlConnection con = Connect();
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = "select * from partido";
+            con.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            List<Evento> eventos = new List<Evento>();
+            while (reader.Read())
+            {
+                eventos.Add(new Evento(reader.GetInt32(0), reader.GetString(1), reader.GetString(2)));
+            }
+            return eventos;
+        }
+    }
+}
